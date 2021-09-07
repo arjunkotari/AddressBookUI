a93915654e19e579c09feced4df118bbb038c9a5
arjunkotari <arjun.kotari123@gmail.com>
2021-09-07 22:12:24 +0530

    Added: Populate the Address Book List from the Local Storage

--- js/home.js
+++ js/home.js
@@ -1,8 +1,13 @@
 window.addEventListener("DOMContentLoaded", (event) => {
+    contactList = getContactFromStorage()
     document.querySelector(".contact-count").textContent = contactList.length;
     createInnerHtml();
 });
 
+const getContactFromStorage = () => {
+    return localStorage.getItem('contactList') ?
+        JSON.parse(localStorage.getItem('contactList')) : []
+}
 
 const createInnerHtml = () => {
     if (contactList.length == 0) {
@@ -21,29 +26,18 @@ const createInnerHtml = () => {
         innerHtml = `${innerHtml} 
           <tr>
           <td>${contact._name}</td>
-          <td>${contact._address}</td>
+          <td>
+              ${contact._address}
+          </td>
           <td>${contact._city}</td>
           <td>${contact._state}</td>
           <td>${contact._zip}</td>
           <td>${contact._phoneNumber}</td>
           <td>
-              <img src="../asserts/delete.jpg" alt="delete" style="height: 40px;" id="${contact._id}" onclick="remove(this)">
-              <img src="../asserts/update.png" alt="update" style="height: 40px;" id="${contact._id}" onclick="update(this)">
-          </td>
+            <img src="../asserts/delete.jpg" alt="delete" style="height: 40px;" id="${contact._id}" onclick="remove(this)">
+            <img src="../asserts/update.png" alt="update" style="height: 40px;" id="${contact._id}" onclick="update(this)">
+        </td>
           </tr>`;
     }
     document.querySelector("#table-display").innerHTML = innerHtml;
 };
-
-let contactList = [
-    {
-        _id: 1629208165495,
-        _name: "Arjun",
-        _phoneNumber: "91 8544787678",
-        _address: "Nizamabad",
-        _city: "Hyderabad",
-        _state: "Telangana",
-        _zip: "517001",
-    },
-    
-];
